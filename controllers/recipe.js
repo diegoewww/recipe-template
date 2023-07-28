83c99dbe342783875236aca6c9dbe19d6725e14a
tuvieja <105611148+n1na666@users.noreply.github.com>
2023-07-28 14:06:42 -0500

    mejorando

--- controllers/recipe.js
+++ controllers/recipe.js
@@ -20,10 +20,13 @@ module.exports = {
     }
   },
   getFavorite: async (req, res) => {
-    console.log(req.user)
     try {
 
-      const recipes = await Favorite.find({ user: req.user.id }).populate('recipe');
+      const recipes = await Favorite.find({ user: req.user.id }).populate('recipe').populate({
+        path: 'recipe',
+        // Get friends of friends - populate the 'friends' array for every friend
+        populate: { path: 'user' }
+      });
 
       console.log(recipes)
       //Sending post data from mongodb and user data to ejs template
@@ -38,7 +41,7 @@ module.exports = {
       //Since we have a session each request (req) contains the logged-in users info: req.user
       //console.log(req.user) to see everything
       //Grabbing just the posts of the logged-in user
-      const recipes = await Recipe.find().sort({ createdAt: "desc" }).lean();
+      const recipes = await Recipe.find().sort({ createdAt: "desc" }).lean().populate('user');
       //Sending post data from mongodb and user data to ejs template
       res.render("feed.ejs", { recipes: recipes });
     } catch (err) {
